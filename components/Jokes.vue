d016dc6eccf0fac610ff23a7749af9a1182d68bb
Xuxin Zhang <70370207+xux1an@users.noreply.github.com>
2021-08-18 20:02:55 +0800

    Set up the nav bar and the content for each pages

---
+++ components/Jokes.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+    <h1>This is the Jokes page</h1>
+    <div v-for="joke in jokes" :key="joke.id">
+        {{joke.joke}}
+
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name:"Jokes",
+    data(){
+        return{
+            jokes:[]
+        }
+    },
+    created(){
+        const config={
+            headers:{
+                Accept:"application/json"
+            }
+            
+        }
+        fetch("https://icanhazdadjoke.com/search", config)
+        .then(res=>res.json())
+        .then(data=>{
+            this.jokes=data.results
+        })
+    
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/jokes/index.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+ <h1>This is the Jokes page</h1>
+  <Jokes/>
+</div>
+ 
+</template>
+
+<script>
+import Jokes from "../../components/Jokes.vue"
+export default {
+    components:{
+        Jokes
+    }
+
+};
+</script>
+
+<style>
+</style>
